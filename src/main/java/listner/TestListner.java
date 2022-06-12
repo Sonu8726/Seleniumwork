701601a7a38d5acdc43ccb29b6777a85346cb8e8
Sonu Kumari <Sonu@SeleniumWork>
2022-06-12 14:02:10 +0530

    Updated the TestListner file to open Report

--- src/main/java/listner/TestListner.java
+++ src/main/java/listner/TestListner.java
@@ -1,5 +1,9 @@
 package listner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -8,13 +12,29 @@ public class TestListner implements ITestListener {
 	@Override
 	public void onFinish(ITestContext arg0) {
 		// TODO Auto-generated method stub
-
+		try {
+			ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",
+					"cd \"C:\\Users\\5onuk\\git\\Seleniumwork\" && allure serve allure-results");
+			builder.redirectErrorStream(true);
+			Process p = builder.start();
+			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line;
+			while (true) {
+				line = r.readLine();
+				if (line == null) {
+					break;
+				}
+				System.out.println(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void onStart(ITestContext arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -38,7 +58,7 @@ public class TestListner implements ITestListener {
 	@Override
 	public void onTestStart(ITestResult arg0) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
